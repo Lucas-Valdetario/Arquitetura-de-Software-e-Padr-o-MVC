78849c60054df2e65393f04f6314332ae67dd5e1
Lucas Valdetario Vieira <lucas.vieira@embrapii.org.br>
2025-05-15 14:23:43 -0300

    Updating

--- src/models/sqlite/repositories/repositories_test.py
+++ src/models/sqlite/repositories/repositories_test.py
@@ -1,6 +1,7 @@
 import pytest
 from src.models.sqlite.settings.connection import db_connection_handler
 from .pets_repository import PetsRepository
+from .people_repository import PeopleRepository
 
 db_connection_handler.connect_to_db()
 
@@ -17,3 +18,19 @@ def test_delete_pet():
 
     repo = PetsRepository(db_connection_handler)
     repo.delete_pets(name)
+
+@pytest.mark.skip(reason="interação com o banco")
+def test_insert_person():
+    first_name = "John"
+    last_name = "Doe"
+    age = 30
+    pet_id = 1
+    repo = PeopleRepository(db_connection_handler)
+    repo.insert_person(first_name, last_name, age, pet_id)
+
+def test_get_person():
+    person_id = 1
+
+    repo = PeopleRepository(db_connection_handler)
+    response = repo.get_person(person_id)
+    print(response)
